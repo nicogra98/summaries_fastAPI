a57bdd6de9cd40c908c19f3de5711d12b5c42908
nicogra <nicogranados1210@gmail.com>
2024-09-16 00:29:11 +0200

    new fewatures and checks and tests

---
+++ project/app/models/pydantic.py
@@ -0,0 +1,11 @@
+from pydantic import AnyHttpUrl, BaseModel
+
+
+class SummaryPayloadSchema(BaseModel):
+    url: AnyHttpUrl
+
+class SummaryResponseSchema(SummaryPayloadSchema):
+    id: int
+
+class SummaryUpdatePayloadSchema(SummaryPayloadSchema):
+    summary: str
